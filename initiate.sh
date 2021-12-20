0124282beb2b032bb758b40cc8dcbf45a224505b
Tuantrung <tuantrung03121997@gmail.com>
2021-12-20 13:52:43 +0700

    fix wrongs file in training script

--- initiate.sh
+++ initiate.sh
@@ -7,7 +7,7 @@ echo "Activate virtual environment"
 cd venv1 && source bin/activate
 
 echo "Install dependency"
-pip install -r requirement.txt
+pip install -r requirements.txt
 
 echo "Run training script"
 python script.py --agent PPO --overlay ./data/trung_network.gpickle --request ./data/requests.json --output output/ --logs logs/
